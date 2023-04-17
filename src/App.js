12ded701de1a6cfa9114b640b99d5121f885a078
Akanksha25052002 <akankshashrivastava2022@vitbhopal.ac.in>
2023-04-17 23:46:38 +0530

    Adding

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import Person from './components/Person';
-import { Fragment } from 'react';
+import Navbar from './components/Navbar';
+import Counter from './components/Counter';
+import { Spinner } from './components/Spinner';
+
+
 
 function App() {
+  
   return (
-    <>
-    hello
-    <person>
-      
-      
-    </person>
 
+    
+    <>
+    <Navbar/>
+    hello
+
+      {/* <Person name={' Akanksha '}  address={' jiwaji ganj '} city={' Gwalior '}/> */}
+
+{/* <Person name=" Akanksha "/>
+<Person name=" yash "/>
+<Person name=" Ansh "/>
+<Person city='gwalior'/> */}
+
+{/* <Person name=" Ansh " city="pune"/>
+<Person name=" vijay " city="Indore"/> */}
+
+<Person/>
+
+<Counter/>
+<Spinner/>
     </>
   );
 }
 
 export default App;
+
+// export const name=(props)=>{
+//   return(
+// <>
+//     {props.fname} {props.age}
+// </>
+//   )
+// }
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,7 +1,19 @@
+/* eslint-disable no-restricted-globals */
 import React from 'react'
 
-export default function Person(props) {
+// export default function Person({name, city, address})
+// export default function Person(props)
+export default function Person()
+{
   return (
-    <div>Person</div>
+    <>
+   {/* {'name'} {'city'} {'address'}  */}
+   {/* {props.name} {props.city}  */}
+   
+
+   <h1>Akanksha</h1>
+   <p>My name is Akanksha Shrivastava I am from Gwalior M.P.</p>
+    
+    </>
   )
 }
